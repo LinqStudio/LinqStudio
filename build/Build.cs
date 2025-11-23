4fb54f5a3918b00ac2e301039f04b429ef2a4bd0
Jordan Fiset <jordanfiset24@hotmail.com>
2025-11-23 09:39:41 -0500

    Introduce NUKE build system and configuration

---
+++ build/Build.cs
@@ -0,0 +1,59 @@
+using Nuke.Common;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using System.Collections.Generic;
+using System.Linq;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+class Build : NukeBuild
+{
+    public static int Main() => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution]
+    readonly Solution Solution;
+
+    // Get all projects except the build project itself
+    IEnumerable<Project> BuildableProjects => Solution.AllProjects.Where(p => p != Solution.GetProject("_build"));
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            // Clean bin/obj folders for all projects except the build project
+            foreach (var project in BuildableProjects)
+            {
+                DotNetClean(s => s
+                    .SetProject(project)
+                    .SetConfiguration(Configuration));
+            }
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetRestore(s => s
+                .SetProjectFile(Solution));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetBuild(s => s
+               .SetProjectFile(Solution)
+               .SetConfiguration(Configuration)
+               .EnableNoRestore());
+        });
+
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(s => s
+                .SetProjectFile(Solution)
+                .EnableNoBuild());
+        });
+}
